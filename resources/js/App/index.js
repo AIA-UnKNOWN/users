d7d3cc9f0d7527859c376edf6205ad59ad56f3c5
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-05-17 12:18:05 +0800

    initialized the app

---
+++ resources/js/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <div className="container">
+        <p className="text-success">Hello Users App</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
